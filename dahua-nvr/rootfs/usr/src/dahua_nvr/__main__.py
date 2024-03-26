29ab52feef916e04893fe9ca8695ce643c7a14a7
Dmitry Bashkatov <dbashkatov@gmail.com>
2024-03-26 19:50:05 +0300

    add dahua-nvr

---
+++ dahua-nvr/rootfs/usr/src/dahua_nvr/__main__.py
@@ -0,0 +1,211 @@
+import re
+import time
+import json
+import logging
+import argparse
+import threading
+from collections import OrderedDict
+
+import paho.mqtt.client as mqtt
+
+from .device import DahuaDevice
+
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+log = logging.getLogger('__name__')
+
+
+DISCOVERY_PREFIX = 'homeassistant'
+NODE_ID = 'dahua_nvr'
+SET_RE = re.compile(rf'{DISCOVERY_PREFIX}/switch/{NODE_ID}/cam(\d+)/set')
+OFF_SUFFIX = '_OFF_'
+UPDATE_INTERVAL = 60
+
+state_changed_event = threading.Event()
+
+
+def read_state(device: DahuaDevice):
+    cameras = device.secure_request('LogicDeviceManager.secGetCameraAll', None)['params']['camera']
+    cameras = filter(lambda c: c['Type'] == 'Remote', cameras)
+    cameras_dict = OrderedDict((c['UniqueChannel'], c) for c in cameras if c.get('Enable'))
+
+    states = device.request('LogicDeviceManager.getCameraState', {'uniqueChannels': [-1]})['params']['states']
+    for s in states:
+        cam = cameras_dict.get(s['channel'])
+        if cam:
+            cam['_state'] = s['connectionState'] == 'Connected'
+
+    for cam in cameras_dict.values():
+        channel0 = cam['UniqueChannel']
+        channel1 = channel0 + 1
+        cam['_name'] = f'cam{channel1}'
+
+    # import pprint
+    # pprint.pprint(cameras)
+    # pprint.pprint(states)
+    # pprint.pprint(cameras_dict)
+
+    return cameras_dict
+
+
+def publish_config(client: mqtt.Client, state):
+    for cam in state.values():
+        cam_name = cam['_name']
+        mqtt_prefix = f'{DISCOVERY_PREFIX}/switch/{NODE_ID}/{cam_name}'
+
+        unique_id = f'{NODE_ID}_{cam_name}'
+        ip = cam['DeviceInfo']['Address']
+
+        entity_config = {
+            'unique_id': unique_id,
+            'name': cam_name,
+            'command_topic': f'{mqtt_prefix}/set',
+            'state_topic': f'{mqtt_prefix}/state',
+            'device': {
+                'model': cam['DeviceInfo']['DeviceType'],
+                'connections': [
+                    ['ip', ip],
+                    ['port', cam['DeviceInfo']['Port']],
+                ],
+            },
+            'icon': 'mdi:video',
+        }
+
+        if ip:
+            entity_config['device']['configuration_url'] = f'http://{ip}/'
+
+        client.publish(f'{mqtt_prefix}/config', json.dumps(entity_config), qos=1)
+        log.info(f'{cam_name} config published')
+
+
+def publish_state(client: mqtt.Client, state):
+    for cam in state.values():
+        cam_name = cam['_name']
+        mqtt_prefix = f'{DISCOVERY_PREFIX}/switch/{NODE_ID}/{cam_name}'
+
+        state_str = 'ON' if cam['_state'] else 'OFF'
+
+        client.publish(f'{mqtt_prefix}/state', state_str, qos=1)
+        log.info(f'{cam_name} state published')
+
+
+def on_connect(client: mqtt.Client, device: DahuaDevice, flags, reason_code, properties):
+    state = read_state(device)
+    publish_config(client, state)
+    publish_state(client, state)
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(f'{DISCOVERY_PREFIX}/switch/{NODE_ID}/+/set')
+
+
+def on_message(client: mqtt.Client, device: DahuaDevice, msg: mqtt.MQTTMessage):
+    match = SET_RE.match(msg.topic)
+    if not match:
+        log.error('Got invalid MQTT topic:', msg.topic)
+        return
+
+    new_state_b = msg.payload
+    if new_state_b == b'ON':
+        new_state = True
+    elif new_state_b == b'OFF':
+        new_state = False
+    else:
+        log.error('Got invalid state:', new_state_b)
+        return
+
+    channel1 = int(match.group(1))
+    channel0 = channel1 - 1
+
+    state = read_state(device)
+    cam = state[channel0]
+    cam_name = cam['_name']
+
+    username = cam['DeviceInfo']['UserName']
+    current_state = not username.endswith(OFF_SUFFIX)
+
+    if new_state == current_state:
+        publish_state(client, state)
+        return
+
+    if new_state:
+        username = username.removesuffix(OFF_SUFFIX)
+    else:
+        username += OFF_SUFFIX
+    cam['DeviceInfo']['UserName'] = username
+
+    # unknown field added on `LogicDeviceManager.secSetCamera`
+    cam['DeviceInfo']['VideoInputChannels'] = None
+
+    device.secure_request('LogicDeviceManager.secSetCamera', {'cameras': [cam]})
+    if new_state:
+        log.info(f'{cam_name} turned on')
+    else:
+        log.info(f'{cam_name} turned off')
+
+    cam['_state'] = new_state
+    publish_state(client, state)
+    state_changed_event.set()
+
+
+def on_message_protected(*args, **kwargs):
+    try:
+        return on_message(*args, **kwargs)
+    except Exception:
+        log.exception('Got error during message handling')
+
+
+class UpdateThread(threading.Thread):
+    def __init__(self, client: mqtt.Client, device: DahuaDevice):
+        super().__init__(daemon=True)
+        self.client = client
+        self.device = device
+
+    def run(self):
+        while True:
+            if state_changed_event.wait(timeout=UPDATE_INTERVAL):
+                state_changed_event.clear()
+                log.info('State changed, will wait for 10 seconds and refresh state from NVR')
+                # if woken by event, give some time for NVR to connect to new camera
+                time.sleep(10)
+            else:
+                log.info('Refreshing state from NVR by timer')
+
+            if self.client.is_connected:
+                publish_state(self.client, read_state(self.device))
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--mqtt-host', required=True)
+    arg_parser.add_argument('--mqtt-port', type=int, default=1883)
+    arg_parser.add_argument('--mqtt-username', required=True)
+    arg_parser.add_argument('--mqtt-password', required=True)
+    arg_parser.add_argument('--dahua-host', required=True)
+    arg_parser.add_argument('--dahua-username', required=True)
+    arg_parser.add_argument('--dahua-password', required=True)
+
+    args = arg_parser.parse_args()
+
+    device = DahuaDevice(args.dahua_host)
+    device.login(args.dahua_username, args.dahua_password)
+
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    mqttc.user_data_set(device)
+    mqttc.on_connect = on_connect
+    mqttc.on_message = on_message_protected
+    mqttc.username_pw_set(args.mqtt_username, args.mqtt_password)
+    mqttc.connect(args.mqtt_host, args.mqtt_port)
+
+    update_thread = UpdateThread(mqttc, device)
+    update_thread.start()
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    mqttc.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
---
+++ dahua-nvr/rootfs/usr/src/dahua_nvr/device.py
@@ -0,0 +1,163 @@
+import json
+import base64
+import random
+import hashlib
+from functools import cached_property
+
+import requests
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+
+
+class DahuaDevice:
+    def __init__(self, host):
+        self.host = host
+        self.uri = f'http://{self.host}/RPC2'
+        self.login_uri = f'http://{self.host}/RPC2_Login'
+        self.last_request_id = 0
+        self.session_id = None
+        self.key = None
+
+    def request(self, method, params, uri=None, raise_for_result=True):
+        if uri is None:
+            uri = self.uri
+
+        self.last_request_id += 1
+
+        body = {
+            'method': method,
+            'params': params,
+            'id': self.last_request_id,
+        }
+
+        headers={}
+
+        if self.session_id:
+            body['session'] = self.session_id
+
+        resp = requests.post(uri, data=json.dumps(body), headers=headers)
+        resp.raise_for_status()
+        resp_body = resp.json()
+        self.session_id = resp_body.get('session')
+
+        if raise_for_result and not resp_body['result']:
+            raise Exception('Request failed', resp_body)
+
+        return resp_body
+
+    def login(self, username, password):
+        dahua_json = self.request('global.login', {
+            "userName": "admin",
+            "password": "",
+            "clientType": "Dahua3.0-Web3.0"
+        }, uri=self.login_uri, raise_for_result=False)
+
+        encryption = dahua_json['params']['encryption']
+
+        if encryption == 'Default':
+            self.key = dahua_md5_hash(dahua_json['params']['random'], dahua_json['params']['realm'], username, password)
+        elif encryption == 'OldDigest':
+            self.key = sofia_hash(password)
+        else:
+            raise Exception('Unknown encryption', encryption)
+
+        resp = self.request('global.login', {
+            "userName": username,
+            "password": self.key,
+            "clientType": "Dahua3.0-Web3.0",
+            "authorityType": "Default",
+            "passwordType": "Default"
+        }, uri=self.login_uri)
+
+        if not resp['result']:
+            raise Exception('Login failed', resp)
+
+        return resp
+
+    def logout(self):
+        if not self.session_id:
+            return
+
+        self.request('global.logout', '')
+        self.last_request_id = 0
+        self.session_id = None
+        del self.rsa_pub_key
+
+    @cached_property
+    def rsa_pub_key(self):
+        encrypt_info = self.request('Security.getEncryptInfo', '')
+        pub_key = {p[0]: p[1] for p in (p.split(':') for p in encrypt_info['params']['pub'].split(','))}
+        return RSA.construct((int(pub_key['N'], 16), int(pub_key['E'], 16)))
+
+    def secure_request(self, method, params, raise_for_result=True):
+        def gen_password(ln):
+            if ln > 16:
+                ln = 16
+            rnd = str(random.random())
+            if rnd[len(rnd)-ln: 1] == '0':
+                return gen_password(ln)
+            else:
+                return rnd[len(rnd)-ln:]
+
+        def pad(data):
+            block_size = 16
+            bytes_to_add = block_size - ((len(data) % block_size) or block_size)
+            return data + (b'\0' * bytes_to_add)
+
+        def unpad(data):
+            while data[-1] == 0:
+                data = data[:-1]
+            return data
+
+        password = gen_password(16).encode('utf-8')
+
+        cipher = PKCS1_v1_5.new(self.rsa_pub_key)
+        salt = cipher.encrypt(password).hex()
+
+        cipher = AES.new(password, AES.MODE_ECB)
+        body = pad(json.dumps(params).encode('utf-8'))
+
+        content = base64.b64encode(cipher.encrypt(body)).decode('ascii')
+
+        ret = self.request(method, {
+            'salt': salt,
+            'cipher': 'AES-128',
+            'content': content,
+        }, raise_for_result=raise_for_result)
+
+        content = base64.b64decode(ret['params']['content'])
+        body = unpad(cipher.decrypt(content))
+
+        params = json.loads(body)
+        ret['params'] = params
+
+        return ret
+
+
+def dahua_md5_hash(dahua_random, dahua_realm, username, password):
+    str1 = username + ':' + dahua_realm + ':' + password
+    hash1 = hashlib.md5(str1.encode()).hexdigest().upper()
+    str2 = username + ':' + dahua_random + ':' + hash1
+    hash2 = hashlib.md5(str2.encode()).hexdigest().upper()
+    return hash2
+
+
+# From: https://github.com/tothi/pwn-hisilicon-dvr
+# Xiongmaitech and Dahua share same 48bit password hash
+def sofia_hash(msg):
+    h = ""
+    m = hashlib.md5()
+    m.update(msg)
+    msg_md5 = m.digest()
+    for i in range(8):
+        n = (ord(msg_md5[2*i]) + ord(msg_md5[2*i+1])) % 0x3e
+        if n > 9:
+            if n > 35:
+                n += 61
+            else:
+                n += 55
+        else:
+            n += 0x30
+        h += chr(n)
+    return h
